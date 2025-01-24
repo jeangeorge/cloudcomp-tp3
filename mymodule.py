eb05e2d544f0708062af7ac38763d48b0152b447
Jean George <jean.george.alves@gmail.com>
2025-01-24 00:35:56 -0300

    use warnings

--- mymodule.py
+++ mymodule.py
@@ -121,11 +121,11 @@ def get_moving_average(input: dict, context: object) -> dict[str, float]:
     return result
             
 def handler(input: dict, context: object) -> dict[str, float]:
-    logger.info(f"[DEBUG] Handler function called with the following input: {input}")
+    logger.warning(f"[WARNING] Handler function called with the following input: {input}")
     result = {
         'percent-network-egress': get_percentage_of_outgoing_network_traffic(input),
         'percent-memory-cache': get_percentage_of_memory_caching_content(input),
         **get_moving_average(input, context)
     }
-    logger.info(f"[INFO] Handler function result: {result}")
+    logger.warning(f"[WARNING] Handler function result: {result}")
     return result
